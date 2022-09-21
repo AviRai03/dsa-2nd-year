820c44ae0652c12fe9d161120587243c7b61a014
AviRai03 <108115255+AviRai03@users.noreply.github.com>
2022-09-21 09:25:28 +0530

    Create basicdatatype.cpp

---
+++ Assignment/Assignment 1/basicdatatype.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;
+    long l;
+    char a;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf",&i,&l,&a,&f,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf",i,l,a,f,d);    
+    return 0;
+}
+
+
+link:https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/submissions/code/289995214
