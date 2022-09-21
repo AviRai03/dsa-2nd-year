2374314b6ff1d78d5281e650304e0813734e1013
AviRai03 <108115255+AviRai03@users.noreply.github.com>
2022-09-21 09:28:41 +0530

    Create conditionalstatement.cpp

---
+++ Assignment/Assignment 1/conditionalstatement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    if (a==1) {
+    cout<<"one";
+    }
+   else if (a==2) {
+   cout<<"two";
+   }
+   else if (a==3) {
+   cout<<"three";
+   }
+   else if (a==4) {
+   cout<<"four";
+   } 
+   else if (a==5) {
+   cout<<"five";
+   }
+   else if (a==6) {
+   cout<<"six";
+   }
+   else if (a==7) {
+   cout<<"seven";
+   }
+   else if (a==8) {
+   cout<<"eight";
+   }
+   else if (a==9) {
+   cout<<"nine";
+   }
+  else {
+      cout<<"Greater than 9";
+  }
+   return 0;
+}
+
+
+link:https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/submissions/code/289992256
