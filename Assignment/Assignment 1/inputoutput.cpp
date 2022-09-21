f186be7f851b6a0f6be579262fbf91a1cd32ba68
AviRai03 <108115255+AviRai03@users.noreply.github.com>
2022-09-21 09:23:53 +0530

    Create inputoutput.cpp

---
+++ Assignment/Assignment 1/inputoutput.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int a,b,c,s;
+   cin>>a>>b>>c;
+   s=a+b+c;
+   cout<<s;
+    return 0;
+}
+ 
+link:https://www.hackerrank.com/challenges/cpp-input-and-output/submissions/code/289712545
